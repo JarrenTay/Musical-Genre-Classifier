1df78b6d82a0c0a45eb8ef145d3efbd7ad0973d1
JarrenTay <jarrentayusa@gmail.com>
2019-04-05 01:48:48 -0400

    Decided how to capture probable chorus of song, and what datapoints to make
    Chorus usually starts at around 25% of the song. Search for segments that
    start at around this time. Use the closest one. If none exist, the just
    start capturing at 25% into the song. Capture until the end of the segment
    or 25 seconds into the segment.
    
    With this time segment, we will create a datapoint for every note in the
    segment. It has the starting note one-hot encoded, four ints or floats that
    describe the change in pitch, the first 3 timbre elements of the first note
    and the probable key of song. Each data point will have 20 dimensions.
    
    Looks something like this:
    {C?, C#?, D?, D#?, E?, F?, F#?, G?, G#?, A?, A#?, B?,
    n0->n1, n1->n2, n2->n3, n3->n4, tim0, tim1, tim2, key}

--- createDataPoints.py
+++ createDataPoints.py
@@ -1,6 +1,8 @@
 import spotipy
 import spotipy.util as util
 import sys
+import heapq
+import subprocess
 from getpass import getpass
 
 #read ssh file to get username, client_id, and client_secret
@@ -10,7 +12,7 @@ username = info.readline()[:-1]
 clientId = info.readline()[:-1]
 clientSecret = info.readline()[:-1]
 redirect = info.readline()[:-1]
-password = getpass()
+#password = getpass()
 
 scope = 'user-library-read app-remote-control streaming'
 
@@ -36,74 +38,19 @@ playlist = spotify.user_playlist_tracks(userId, playlistId)
 numTracks = playlist['total']
 tracks = playlist['items']
 
-trackId = tracks[9]['track']['id']
+trackId = tracks[int(sys.argv[1])]['track']['id']
 print trackId
 
-#spotify.start_playback()
-#aa = spotify.audio_analysis(trackId)
-aa = spotify.audio_analysis('7e7VjLxO5xJINHvnRytrqi')
-#print aa['sections']
-#print len(aa['sections'])
+aa = spotify.audio_analysis(trackId)
+track = spotify.track(trackId)
+print track['name']
 
-secLoudness = -100.0
-numSecs = len(aa['sections'])
-secStarts = list()
-for x in xrange(numSecs):
-	print aa['sections'][x]['start']
-	secStarts.append(aa['sections'][x]['start'])
+posToNote = {0: 'C', 1: 'C#', 2: 'D', 3: 'D#', 4: 'E', 5: 'F', 6: 'F#', 7: 'G', 8: 'G#', 9: 'A', 10: 'A#', 11: 'B'}
+posToNote2 = {0: 'C', 1: 'c', 2: 'D', 3: 'd', 4: 'E', 5: 'F', 6: 'f', 7: 'G', 8: 'g', 9: 'A', 10: 'a', 11: 'B'}
 
-print secStarts
-
-divList = list()
-for _ in xrange(len(aa['sections']) + 1):
-	divList.append(list())
-
-numSegs = len(aa['segments'])
-for y in xrange(numSegs):
-	for z in xrange(len(secStarts)):
-		#print aa['segments'][y]['start'], secStarts[z]
-		if (aa['segments'][y]['start'] < secStarts[z]):
-			sectionNum = z - 1;
-			break
-		sectionNum = z
-	count = 0
-	for note in aa['segments'][y]['pitches']:
-		if note == 1.0:
-			divList[sectionNum].append(count)
-			break
-		count = count + 1
-
-printed = True
-oString = ''
-index = 0
-while(printed):
-	printed = False
-	for lis in divList:
-		if len(lis) > (index + 1):
-			printed = True
-			oString = oString + str(lis[index]) + '\t'
-		else:
-			oString = oString + '\t'
-	oString = oString + '\n'
-	index = index + 1
-	
-print oString
-
-count = 0
-#for seg in aa['segments']:
-	#if seg['start'] > loudSecBegin and seg['start'] < loudSecEnd:
-	#	count = count + 1
-	#	print seg['pitches']
-	#print seg['timbre']
-#print count
 # C, C#, D, D#, E, F, F#, G, G#, A, A#, B
-# lots of info here. What do we want?
-# We have number of segments
-# we have note chroma of each note
-# We have timbre of each note
-# We have time between each note
+# Chorus is usually around 25% of the songs duration
+# Let's determine this time, and give a 5% leniency for segment timing.
+# Cap the chorus at 25 seconds.
+# Create datapoints that that has a starting note (one-hot encoded), four ints that describe increase or decrease in pitch, the timbre of the first note, and the key of the song.
 
-# Basic notes: 5 notes, 4 times, 3 timbres per note? => 24 dimensions per data point
-# High bpm 2:36 song has 787 segments. 782 points. Thats a lot of points per song
-
-# Use loudest section? Loudest section was 50 secs. had 175 segments. 
