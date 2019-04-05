d562de0392637a9149e8be3a3aa89443e05721cb
JarrenTay <jarrentayusa@gmail.com>
2019-04-05 03:29:10 -0400

    Can now create datapoints for the entire playlist list.

--- createDataPoints.py
+++ createDataPoints.py
@@ -25,96 +25,97 @@ else:
 
 spotify = spotipy.Spotify(auth=token)
 
-playlistList = open('playlistList.txt', 'r')
+playlistListFile = open('playlistList.txt', 'r')
 
-line = playlistList.readline()[:-1]
-lineList = line.split(',')
-category = lineList[0]
-infoList = lineList[1].split(':')
-userId = infoList[2]
-playlistId = infoList[4]
+playlistListRaw = playlistListFile.read()
 
-playlist = spotify.user_playlist_tracks(userId, playlistId)
-numTracks = playlist['total']
-tracks = playlist['items']
-
-trackId = tracks[int(sys.argv[2])]['track']['id']
-print trackId
-
-aa = spotify.audio_analysis(trackId)
-track = spotify.track(trackId)
-print track['name']
-
-# C, C#, D, D#, E, F, F#, G, G#, A, A#, B
-# Chorus is usually around 25% of the songs duration
-# Let's determine this time, and give a 5% leniency for segment timing.
-# Cap the chorus at 25 seconds.
-# Create datapoints that that has a starting note (one-hot encoded), four ints that describe increase or decrease in pitch, the timbre of the first note, and the key of the song.
-
-melodyStartGuess = track['duration_ms'] / 4000
-melodyThreshold = (track['duration_ms'] - (track['duration_ms'] * 0.95)) / 1000
-
-closestSection = None
-closestSectionStart = melodyStartGuess
-closestSectionEnd = melodyStartGuess + 25
-closestSectionDist = melodyThreshold
-for section in aa['sections']:
-    sectionDist = abs(section['start'] - melodyStartGuess)
-    if sectionDist < closestSectionDist:
-        closestSection = section
-        closestSectionStart = section['start']
-        closestSectionDist = sectionDist
-        if section['duration'] < 25:
-            closestSectionEnd = section['start'] + section['duration']
-        else:
-            closestSectionEnd = section['start'] + 25
-
-if closestSection == None:
-    for section in aa['sections']:
-        sectionEnd = section['start'] + section['duration']
-        if (sectionEnd) > closestSectionStart:
-            closestSection = section
-            break
-
-print(closestSectionStart, closestSectionEnd, closestSectionDist)
-
-with open(sys.argv[1], 'w+') as dataFile:
-    for segmentNum in xrange(len(aa['segments']) - 4):
-        #print segmentNum
-        segment = aa['segments'][segmentNum]
-        dataString = ''
-        if segment['start'] > closestSectionStart:
-            if segment['start'] < closestSectionEnd:
-                noteCount = 0
-                noteNum = -1
-                for note in segment['pitches']:
-                    if note == 1.00:
-                        noteNum = noteCount
-                        dataString+=('1,')
+playlistList = playlistListRaw.split('\n')
+for playlistRaw in playlistList:    
+    lineList = playlistRaw.split(',')
+    category = lineList[0]
+    infoList = lineList[1].split(':')
+    userId = infoList[2]
+    playlistId = infoList[4]
+    
+    playlist = spotify.user_playlist_tracks(userId, playlistId)
+    numTracks = playlist['total']
+    trackList = playlist['items']
+    
+    with open('data/' + category + '.data', 'a+') as dataFile:
+        for track in trackList:
+            trackId = track['track']['id']
+            print trackId
+            
+            aa = spotify.audio_analysis(trackId)
+            track = spotify.track(trackId)
+            print track['name']
+            
+            # C, C#, D, D#, E, F, F#, G, G#, A, A#, B
+            # Chorus is usually around 25% of the songs duration
+            # Let's determine this time, and give a 5% leniency for segment timing.
+            # Cap the chorus at 25 seconds.
+            # Create datapoints that that has a starting note (one-hot encoded), four ints that describe increase or decrease in pitch, the timbre of the first note, and the key of the song.
+            
+            melodyStartGuess = track['duration_ms'] / 4000
+            melodyThreshold = (track['duration_ms'] - (track['duration_ms'] * 0.95)) / 1000
+            
+            closestSection = None
+            closestSectionStart = melodyStartGuess
+            closestSectionEnd = melodyStartGuess + 25
+            closestSectionDist = melodyThreshold
+            for section in aa['sections']:
+                sectionDist = abs(section['start'] - melodyStartGuess)
+                if sectionDist < closestSectionDist:
+                    closestSection = section
+                    closestSectionStart = section['start']
+                    closestSectionDist = sectionDist
+                    if section['duration'] < 25:
+                        closestSectionEnd = section['start'] + section['duration']
                     else:
-                        dataString+=('0,')
-                    noteCount = noteCount + 1
-                print noteNum
-                for nextSegNums in xrange(1, 5):
-                    nextSegment = aa['segments'][segmentNum + nextSegNums]
-                    noteCount = 0
-                    for note in nextSegment['pitches']:
-                        if note == 1.00:
-                            noteDiff = noteCount - noteNum
-                            if noteDiff > 6:
-                                noteDiff = -(noteDiff - 6)
-                            elif noteDiff < -6:
-                                noteDiff = -(noteDiff + 6)
-                            dataString+=(str(noteDiff) + ',')
-                            noteNum = noteCount
-                            break
-                        noteCount = noteCount + 1
-                for timNum in xrange(3):
-                    dataString+=(str(segment['timbre'][timNum]) + ',')
-                dataString+=(str(closestSection['key']) + '\n')
-            else:
-                break
-        dataFile.write(dataString)
+                        closestSectionEnd = section['start'] + 25
+            
+            if closestSection == None:
+                for section in aa['sections']:
+                    sectionEnd = section['start'] + section['duration']
+                    if (sectionEnd) > closestSectionStart:
+                        closestSection = section
+                        break
+            
+            for segmentNum in xrange(len(aa['segments']) - 4):
+                #print segmentNum
+                segment = aa['segments'][segmentNum]
+                dataString = ''
+                if segment['start'] > closestSectionStart:
+                    if segment['start'] < closestSectionEnd:
+                        noteCount = 0
+                        noteNum = -1
+                        for note in segment['pitches']:
+                            if note == 1.00:
+                                noteNum = noteCount
+                                dataString+=('1,')
+                            else:
+                                dataString+=('0,')
+                            noteCount = noteCount + 1
+                        for nextSegNums in xrange(1, 5):
+                            nextSegment = aa['segments'][segmentNum + nextSegNums]
+                            noteCount = 0
+                            for note in nextSegment['pitches']:
+                                if note == 1.00:
+                                    noteDiff = noteCount - noteNum
+                                    if noteDiff > 6:
+                                        noteDiff = -(noteDiff - 6)
+                                    elif noteDiff < -6:
+                                        noteDiff = -(noteDiff + 6)
+                                    dataString+=(str(noteDiff) + ',')
+                                    noteNum = noteCount
+                                    break
+                                noteCount = noteCount + 1
+                        for timNum in xrange(3):
+                            dataString+=(str(segment['timbre'][timNum]) + ',')
+                        dataString+=(str(closestSection['key']) + '\n')
+                    else:
+                        break
+                dataFile.write(dataString)
 
                 
 
